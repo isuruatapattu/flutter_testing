bddf6f42066acfe6e2989f628fd3c1250ec18e38
Isuru Athapattu <isuruatapattu95@gmail.com>
2022-12-29 22:16:22 +0200

    updated message

--- sample_project/lib/main.dart
+++ sample_project/lib/main.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter!',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter...!'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        )
-      )
-    );
+        title: 'Welcome to Flutter!',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Welcome to Flutter...!'),
+            ),
+            body: const Center(
+              child: Text(
+                  'Hello World - This is to check VS between Android Studio and VS Code'),
+            )));
   }
 }
