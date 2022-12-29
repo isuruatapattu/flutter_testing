c7db347c854a517d0d0f2d256c6b98a1a9aa91b6
Isuru Athapattu <isuruatapattu97@gmail.com>
2022-12-29 23:07:39 +0200

    message change

--- sample_project/lib/main.dart
+++ sample_project/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
             ),
             body: const Center(
               child: Text(
-                  'Hello World - This is to check VS between Android Studio and VS Code'),
+                  'Hello World - This is to check Version Control between Android Studio and VS Code'),
             )));
   }
 }
